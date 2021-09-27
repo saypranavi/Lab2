4b759fc9c59f9683f9792cf8b4b57942a791e4e3
Pranavi Sayyaparaju <91503364+saypranavi@users.noreply.github.com>
2021-09-27 23:26:16 +0000

    bh

--- fade.py
+++ fade.py
@@ -34,10 +34,10 @@ def fadeBlue():
     while 1:
       if GPIO.input(inBlue) == GPIO.HIGH:
         pwm2.start(0)                  # initiate PWM at 0% duty cycle
-        for dc in range(101):       # loop duty cycle from 0 to 100
+        for dc in range(100, 0 , -1):       # loop duty cycle from 0 to 100
           pwm2.ChangeDutyCycle(dc)   # set duty cycle
           sleep(0.01)               # sleep 10 ms
-        for dc in range(100, 0, -1):       # loop duty cycle from 0 to 100
+        for dc in range(0, 100, 1):       # loop duty cycle from 0 to 100
           pwm2.ChangeDutyCycle(dc)   # set duty cycle
           sleep(0.01) 
       pwm2.stop()
