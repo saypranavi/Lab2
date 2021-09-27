c7325bee9fe25d62550d4d0ee7c1b0a6e40e0189
Pranavi Sayyaparaju <91503364+saypranavi@users.noreply.github.com>
2021-09-27 21:52:42 +0000

    cleanup

--- fade.py
+++ fade.py
@@ -1,9 +1,8 @@
 import RPi.GPIO as GPIO 
-GPIO.setmode(GPIO.BCM)
-
-import RPi.GPIO as gpio
-import time# Define input port numbers:
+import time # Define input port numbers:
 from time import sleep
+
+GPIO.setmode(GPIO.BCM)
 
 def blink():
   pBlink = 26    # GPIO pin number
@@ -18,47 +17,40 @@ def blink():
   except KeyboardInterrupt:   # stop gracefully on ctrl-C
     print('\nExiting')
     
-  pwm.stop()
-  GPIO.cleanup()
+def fadeBlue():
+  p = 19
+  GPIO.setup(p, GPIO.OUT)
+  pwm = GPIO.PWM(p, 100)          # create PWM object @ 100 Hz
 
-blink()
+  try:
+    pwm.start(0)                  # initiate PWM at 0% duty cycle
+    while 1:
+      for dc in range(101):       # loop duty cycle from 0 to 100
+        pwm.ChangeDutyCycle(dc)   # set duty cycle
+        sleep(0.01)               # sleep 10 ms
+      for dc in range(100, 0, -1):       # loop duty cycle from 0 to 100
+        pwm.ChangeDutyCycle(dc)   # set duty cycle
+        sleep(0.01) 
+  except KeyboardInterrupt:       
+    print('\nExiting')
 
-# Fade
+fadeBlue()
 
-GPIO.setmode(GPIO.BCM)
+#   pwm.stop()
+#   GPIO.cleanup()
 
-p = 19
-GPIO.setup(p, GPIO.OUT)
-pwm = GPIO.PWM(p, 100)          # create PWM object @ 100 Hz
+#   # Blink
+#   in1, in2 = 21, 24
+#   GPIO.setmode(GPIO.BCM)
+#   GPIO.setup(in1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+#   GPIO.setup(in2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
 
-try:
-  pwm.start(0)                  # initiate PWM at 0% duty cycle
-  while 1:
-    for dc in range(101):       # loop duty cycle from 0 to 100
-      pwm.ChangeDutyCycle(dc)   # set duty cycle
-      sleep(0.01)               # sleep 10 ms
-    for dc in range(100, 0, -1):       # loop duty cycle from 0 to 100
-      pwm.ChangeDutyCycle(dc)   # set duty cycle
-      sleep(0.01) 
-except KeyboardInterrupt:       
-  print('\nExiting')
+# def myCallback(pin):
+#     print("Rising edge detected on pin %d"% pin)# Execute myCallback() if port 1 goes HIGH:
+#   GPIO.add_event_detect(in1, GPIO.RISING, callback=myCallback, bouncetime=200)# Infinite loop:
+#   while True:
+#     print('.', end='')
+#     time.sleep(0.1)
+#   GPIO.cleanup()
 
-pwm.stop()
-GPIO.cleanup()
-
-# Blink
-
-in1, in2 = 21, 24
-gpio.setmode(gpio.BCM)
-gpio.setup(in1, gpio.IN, pull_up_down=gpio.PUD_DOWN)
-gpio.setup(in2, gpio.IN, pull_up_down=gpio.PUD_DOWN) 
-
-def myCallback(pin):
-  print("Rising edge detected on pin %d"% pin)# Execute myCallback() if port 1 goes HIGH:
-gpio.add_event_detect(in1, gpio.RISING, callback=myCallback, bouncetime=200)# Infinite loop:
-while True:
-  print('.', end='')
-  time.sleep(0.1)
-gpio.cleanup()
-
-myCallback()
+#   myCallback()
